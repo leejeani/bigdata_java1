6bb05bf136cc49fa8acf85efae55d672dbdae060
student <student@M704INS>
2017-12-21 11:42:04 +0900

    day01

---
+++ src/com/sds/dev/Hello.java
@@ -0,0 +1,11 @@
+package com.sds.dev;
+
+public class Hello {
+	static public void staticmain(String[] a) {
+		/*
+		 * 2017.7.4 jmlee 변수선언
+		 */
+		int data = 0;
+		System.out.println(data);
+	}
+}
---
+++ src/day01/Hello.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		//변수를 선언
+		int a = 10;
+
+		
+		String str = "ABC";
+		// 연산
+		//a = a + 200;
+		// 출력
+		System.out.println(str);
+		System.out.println(a);
+		
+	}
+}
